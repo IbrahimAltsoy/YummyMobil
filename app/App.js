4958eecad6c25d2da20e5e993afd864d57ea2c8d
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2024-12-17 00:26:50 +0300

    Remove node_modules from Git tracking

--- app/App.js
+++ app/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Router from "./navigations/Router";
 import { NavigationContainer } from "@react-navigation/native";
-// import { StatusBar } from "react-native";
+
 const App = () => {
   return (
     <NavigationContainer>
