9cef122c56142fc570e57857e765d32db7a55006
IbrahimAltsoy <78874266+IbrahimAltsoy@users.noreply.github.com>
2024-12-17 00:30:23 +0300

    Remove node_modules from previous commits

--- app/App.js
+++ app/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Router from "./navigations/Router";
 import { NavigationContainer } from "@react-navigation/native";
-
+ 
+// değişikliklr 
 const App = () => {
   return (
     <NavigationContainer>
